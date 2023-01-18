2be7859d365f9a7d3d0517f5f5007b58acbdecd3
jazocar <jazocar@gasco.cl>
2023-01-18 16:03:51 -0300

    first commit

---
+++ src/main/java/cl/duoc/integracionwebpayamb/IntegracionWebpayAmbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionwebpayamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionWebpayAmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionWebpayAmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/integracionwebpayamb/IntegracionWebpayAmbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionwebpayamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracionWebpayAmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
